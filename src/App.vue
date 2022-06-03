5051ae8f7c55ecc4dfc83e9478bc71fe1ac53f04
aimeiyijia <1456723200@qq.com>
2022-06-03 17:27:29 +0800

    chore：调整主题颜色

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import ChatContent from "./views/chat/index.vue";
+import ChatContent from "./views/chat/index.vue"
+import { NConfigProvider, GlobalThemeOverrides } from "naive-ui"
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    baseColor: "#409EFF",
+    primaryColor: "#409EFF",
+    primaryColorHover: "#409EFF",
+    primaryColorPressed: "#409EFF",
+    primaryColorSuppl: "#409EFF",
+  },
+}
 </script>
 
 <template>
-  <chat-content />
+  <n-config-provider class="app-container" :theme-overrides="themeOverrides">
+    <chat-content />
+  </n-config-provider>
 </template>
 
 <style>
